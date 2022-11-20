2564a42ffc562fc615101335165b033557361a87
sashilshrestha <sashilstha5@gmail.com>
2022-11-20 23:52:50 +0545

    fix dom load

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+document.addEventListener('DOMContentLoaded', function () {
+  /* Responsive Navigation Toggling */
+  const navSlide = () => {
+    const burger = document.querySelector('.all-lines');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      // Burger Animation
+      burger.classList.toggle('toggle');
+    });
+  };
+  navSlide();
+});

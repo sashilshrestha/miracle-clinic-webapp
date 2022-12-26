579b6e94e133e0a27da74e15a1e6bb15d1649d0b
sashilshrestha <sashilstha5@gmail.com>
2022-12-26 22:44:48 +0545

    some completed

--- single-blogs.php
+++ single-blogs.php
@@ -29,7 +29,30 @@
           Tomasz Nieszporski, 1C Entertainment management board member, said in a statement, “We are truly excited by this deal. It is an amazing opportunity for 1CE to achieve our wildest dreams and aspirations in the video games business. Our vision and passion will be now backed by a leading global games company, and I am humbled by the unique chance of working together with Tencent and the other top creative minds that are part of its global ecosystem.”
 
         </div>
-        <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-4">2</div>
+        <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-4">
+          <aside>
+            <div class="share-post">
+              <span class="font-bold mr-2 text-xl">Share on:</span>
+              <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.png" alt=""></a>
+              <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/twitter.png" alt=""></a>
+              <a href=""><img src="<?php echo get_template_directory_uri(); ?>/assets/images/linkedin.png" alt=""></a>
+            </div>
+            <div class="other-posts">
+              <?php
+              for ($x = 0; $x < 4; $x++) {
+              ?>
+                <div class="post-card">
+                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/skin.png" alt="" class="rounded-md mr-4">
+                  <div class="post-description">
+                    <h5>Yarsa Games: A Nepali Mobile Gaming App that Crossed 100 M+ Downloads</h5>
+                  </div>
+                </div>
+              <?php
+              }
+              ?>
+            </div>
+          </aside>
+        </div>
       </div>
     </div>
   </section>
---
+++ template/contact.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Template Name: Contact Page
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+<header class="page--header">
+  <div class="container mx-auto">
+    <h1>Contact Us</h1>
+    <p>Take the first step towards your transformation</p>
+  </div>
+</header>
+<main id="contact-page">
+  <div class="container mx-auto">
+    <div class="content grid grid-cols-12 gap-12 pt-6">
+      <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-5 contact-left">
+        <div class="detail">
+          <h5>Contact Us</h5>
+          <p>Take the first step towards your transformation
+            You can Email us for specific inquiries or Call or text us by phone for any inquiries. You can also send a Call-Back Request. We will get to you soon.</p>
+        </div>
+        <div class="detail">
+          <h5>Enquiries</h5>
+          <p>Telephone Call: +977 01-5919826</p>
+          <p>Cellphone Call/Text: +977 9817125774</p>
+          <p>Email: miracleskinhair@gmail.com</p>
+        </div>
+        <div class="detail">
+          <h5>Opening Hours</h5>
+          <p>Sunday to Friday: 9am to 7pm</p>
+          <p>Saturday: 11am to 5pm</p>
+        </div>
+      </div>
+      <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-7 contact-right">
+        p
+      </div>
+    </div>
+  </div>
+  <div id="map">
+    <div id="top-header">
+      <div class="container mx-auto">
+        <h2>Map Find Us</h2>
+      </div>
+    </div>
+    <div class="map-content">
+      <div class="overlay"></div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.936162419961!2d85.34951711539824!3d27.719257231574602!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1997bfa8f5a1%3A0x109668485a4871e!2sMiracle%20Skin%20Hair%20%26%20Aesthetic%20Clinic!5e0!3m2!1sen!2snp!4v1669397703097!5m2!1sen!2snp" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</main>
+<?php
+get_footer();

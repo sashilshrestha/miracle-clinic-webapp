fa8045ab6bae356fc1a1fd6097933bbd3924b5c2
sashilshrestha <sashilstha5@gmail.com>
2023-01-08 22:51:51 +0545

    test form

--- template/career.php
+++ template/career.php
@@ -9,32 +9,26 @@ defined('ABSPATH') || exit;
 
 get_header();
 ?>
-<header class="page--header mb-20">
+<header class="page--header">
   <div class="container mx-auto">
     <h1>Culture Empowering Transformation</h1>
     <p>We believe in a work environment where our team feels supported, respected and understood.</p>
   </div>
 </header>
-<main id="career-page">
+<!-- <main id="career-page">
   <div class="container mx-auto">
-    <div id="offers">
+    <div id="offers" class="my-20">
       <div class="offers-content">
         <div class="container mx-auto">
           <div class="flex flex-wrap gap-6 justify-center">
             <?php
-            for ($x = 0; $x < 5; $x++) {
+            for ($x = 0; $x < 2; $x++) {
             ?>
               <div class="card">
                 <div>
-                  <h1>Save 15% On</h1>
-                  <h2>Acne Scar Removal</h2>
-                  <h6>Remove acne to reveal your healthy glow of the skin.
+                  <h1>Laser Treatment Expert</h1>
+                  <h6>Posted on: 12th June, 2020
                   </h6>
-                </div>
-                <div>
-                  <div class="book-now-icon">
-                    <h3>Book Now</h3>
-                  </div>
                 </div>
               </div>
             <?php
@@ -45,8 +39,11 @@ get_header();
       </div>
     </div>
   </div>
+</main> -->
+<main class="text-center text-3xl font-bold py-10">
+  <h3>No available Jobs</h3>
 </main>
-<div id="queries" class="mt-20">
+<div id="queries">
   <div class="container mx-auto flex flex-col items-center">
     <h1 class="text-center !text-4xl" style="line-height: 140% !important">Can’t find the position you’re looking for? Send your resume and we’ll contact you when the next post relevant to your CV, is vacant.</h1>
     <button class="button primary !rounded-full"><a href="#" class="text-2xl">hr@miracle.com</a></button>
--- template/contact.php
+++ template/contact.php
@@ -17,7 +17,7 @@ get_header();
 </header>
 <main id="contact-page">
   <div class="container mx-auto">
-    <div class="content grid grid-cols-12 gap-12">
+    <div class="content grid grid-cols-12">
       <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-5 contact-left">
         <div class="detail">
           <h5>Contact Us</h5>
@@ -37,7 +37,9 @@ get_header();
         </div>
       </div>
       <div class="col-span-12 sm:col-span-12 md:col-span-6 lg:col-span-6 xl:col-span-6 2xl:col-span-7 contact-right">
-
+        <div class="px-10 py-6">
+          <?php echo do_shortcode('[wpforms id="247" title="false"]'); ?>
+        </div>
       </div>
     </div>
   </div>

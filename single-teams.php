56233266eacd4c88ad0e73eab31e4c8c2ba8437a
sashilshrestha <sashilstha5@gmail.com>
2023-01-27 10:51:27 +0545

    wip teams

--- single-teams.php
+++ single-teams.php
@@ -1,13 +1,20 @@
 <?php
 get_header();
+$thumb_id = get_post_thumbnail_id();
+$thumb_url = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true);
 ?>
 <main id="single-teams">
   <header class="page--header">
     <div class="container mx-auto">
+      <div class="person-container">
+        <div class="person-circle">
+          <img src="<?php echo $thumb_url[0] ?>" alt="">
+        </div>
+      </div>
       <h1>
-        Pokemon Sword and Shield Are Holding a New Gigantamax Pokemon Event
+        <?php the_title(); ?>
       </h1>
-      <p>February 25, 2022</p>
+      <?php echo get_field('team_designation') ?>
     </div>
   </header>
   <section class="mx-auto container py-10">
